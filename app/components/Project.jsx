109fc134dad92e80fa56f0e5cfd1d3e32e0bf9c1
Muhammad Noman <mhmdnoman01@gmail.com>
2025-04-15 14:47:24 +0600

    small updates

--- app/components/Project.jsx
+++ app/components/Project.jsx
@@ -5,14 +5,17 @@ import React from "react";
 function Project(props) {
   const data = props.data;
   return (
-    <div className="max-w-[460px] bg-primary-50">
+    <div className="max-w-[460px] rounded-md shadow-xs border border-primary-50">
       <Link target="_blank" href={data.link}>
-        <Image
-          src={data.image}
-          width={460}
-          height={299}
-          alt="Profile Picture"
-        />
+        <div className="w-full relative mx-auto h-auto overflow-hidden rounded-lg">
+          <Image
+            className="w-full h-auto relative z-0 rounded-lg transition-all duration-400 hover:scale-110"
+            src={data.image}
+            width={460}
+            height={299}
+            alt="Project Image"
+          />
+        </div>
         <div className="p-3">
           <p className="text-2xl mb-2 hover:text-primary-500">{data.title}</p>
           <p>{data.details}</p>
